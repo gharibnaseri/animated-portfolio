c5bb5fa32a9f94dd1f942c73bca5bf522a44d8fb
mojtaba <110784968+gharibnaseri@users.noreply.github.com>
2024-02-03 10:38:36 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import './app.scss'
+import Navbar from './component/navbar/Navbar';
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar />
+    </section>
+    <section>hero</section>
+    <section>parallex</section>
+    <section>services</section>
+    <section>parallex</section>
+    <section>portfolio1</section>
+    <section>portfolio2</section>
+    <section>portfolio3</section>
+    <section>contact</section>
+    </div>;
 };
 
 export default App;
